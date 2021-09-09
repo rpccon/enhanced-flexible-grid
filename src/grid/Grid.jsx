81859caa2c94feeee26431dfbdda9e4dda9a7387
Roberto Salazar <roberto.salazar@hangarww.com>
2021-09-09 00:54:45 -0600

    ENH:001: Add enhancer flexible grid

---
+++ src/grid/Grid.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Grid.css";
+
+const Grid = () => {
+    return (
+        <div class="grid-container">
+            <div class="grid-item">
+                <div class="grid-container">
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>         
+                </div>
+            </div>
+            <div class="grid-item">
+                <div class="grid-container">
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>             
+                </div>
+            </div>
+            <div class="grid-item">
+                <div class="grid-container">
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>              
+                </div>
+            </div>
+            <div class="grid-item">
+                <div class="grid-container">
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>
+                    <div class="grid-package">
+                        <div class="grid-item last">Cell</div>
+                        <div class="grid-item last">Cell</div>
+                    </div>           
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Grid;
